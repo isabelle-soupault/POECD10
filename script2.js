13de3c69c9775f062e031a0967f22b8888f8d0cd
Isabelle Soupault <isabelle.soupault@gmail.com>
2021-05-20 10:15:10 +0200

    Exercice2 - Afficher 3 variables l'une en dessous de l'autre

---
+++ script2.js
@@ -0,0 +1,7 @@
+const firstName = "John";
+const lastName = "Doe";
+const city = "New York";
+
+alert("Nom :" + " " + lastName + "\n"
+    + "Prénom :" + " " + firstName + "\n"
+    + "Ville :" + " " + city);
