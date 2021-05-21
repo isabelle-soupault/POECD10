9cc4db203c5ab482956b475e79f6949f43f07511
Isabelle Soupault <isabelle.soupault@gmail.com>
2021-05-21 12:12:17 +0200

    Ajout des énoncés

--- script7.js
+++ script7.js
@@ -1,5 +1,11 @@
 submit.addEventListener("click", function () {
-    let result = ((((shoeSize.value * 2) + 5) * 50) - yearOfBirth.value) + 1766;
-    alert(result);
-
+    if (isNaN(shoeSize.value) == true) {
+        alert('Votre pointure doit est un nombre :)');
+    }
+    else if (isNaN(yearOfBirth.value) == true) {
+        alert('Votre année de naissance doit être un nombre :)');
+    } else {
+        let result = ((((shoeSize.value * 2) + 5) * 50) - yearOfBirth.value) + 1766;
+        alert(result);
+    }
 });
