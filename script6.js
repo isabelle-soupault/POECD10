970a2f16f0cbeedb8112c1513714ad54e3c8146a
Isabelle Soupault <isabelle.soupault@gmail.com>
2021-05-25 16:14:53 +0200

    ajout de la correction de l'exo 6

--- script6.js
+++ script6.js
@@ -38,3 +38,37 @@ alert(x); */
 //     alert(result);
 // };
 
+
+//________________________//
+//                        //
+//      CORRECTION        //
+//________________________//
+
+/*
+Utilisation des REGEX (expressions régulières) pour cette correction
+
+1 - Ajouter une fonction dans le formulaire d'envoie
+2 - Dans
+Function submitInfo(){
+    let firstNumber = document.getElementById("firstNumber").value;
+    let secondNumber = document.getElementById("secondNumber").value;
+    const numberRegex=/^[0-9]+(\.[0-9]+)?$/;
+
+    if(numberRegex.test(firstNumber) && numberRegex.test(secondNumber)){
+        let result = firstNumber.value % secondNumber.value;
+        let p = document.createElement("p");
+        p.innerText = result;
+        body.appendChild(p);
+    } else {
+        let p = document.createElement("p");
+        p.innerText = "Ce n'est pas bon";
+        body.appendChild(p);
+
+    };
+
+}
+
+
+
+
+*/
